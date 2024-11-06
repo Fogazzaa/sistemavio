cead3486ba8787de9d9756eaa78038fce6c186a2
fogazza <vinicius.cintra2801@gmail.com>
2024-11-06 09:11:55 -0300

    06-11-24

---
+++ api/src/controllers/eventoController.js
@@ -0,0 +1,25 @@
+const connect = require("../db/connect");
+
+module.exports = class eventoController {
+  // criação de um evento
+  static async createEvento(req, res) {
+    const { nome, descricao, data_hora, local, fk_id_organizador } = req.body;
+
+    if (!nome || !descricao || !data_hora || !local || !fk_id_organizador) {
+      return res
+        .status(400)
+        .json({ error: "Todos os campos devem ser preenchidos" });
+    }
+    const query = ` INSERT INTO evento (nome,descricao,data_hora,local,fk_id_organizador) VALUES (?,?,?,?,?)`;
+    const values = [nome, descricao, data_hora, local, fk_id_organizador];
+    try {
+      connect.query(query, values, (err) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({ error: "Erro ao criar evento!" });
+        }
+        return res.status(201).json({ message: "Evento criado com sucesso!" });
+      });
+    } catch (error) {}
+  }
+};
